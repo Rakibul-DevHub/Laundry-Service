50d18813231cec6a68ab1f914f7005c77a2c32d9
Rakibul-DevHub <smrat246@gmail.com>
2026-09-03 23:18:12 +0600

    fixing Ui bug_

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 import 'app/app.dart';
 
 Future<void> main() async {
